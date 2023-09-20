bfc9c21bb18ee5ae2e5fe9f11ed0cd9a2966135b
Sajjaduj Jaman Emon <mdsajjadujjaman09@gmail.com>
2023-09-20 20:48:16 +0600

    second commit

--- src/__form__.js
+++ src/__form__.js
@@ -3,5 +3,9 @@
 1. use form tag and onSubmit handler with event(e) 
 From e access e.target.[name of the input field] and get value
 
-2. use  individual field state
+2. controlled element: use individual field state for each input field
+
+3. controlled element : one object in a state holding all the input fields value
+
+4. uncontrolled element : 
 */ 
